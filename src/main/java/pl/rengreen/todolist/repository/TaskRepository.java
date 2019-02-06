2fed4172417602b6bbb269e5220f9eaf52b792be
rengreen <r.m.gardzinska@gmail.com>
2019-02-06 21:04:47 +0100

    adding TaskRepository which extends JpaRepository

---
+++ src/main/java/pl/rengreen/todolist/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package pl.rengreen.todolist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.rengreen.todolist.domain.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
