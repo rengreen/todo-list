30c225a064ccd588785e263853dd0c3cc1abd346
rengreen <r.m.gardzinska@gmail.com>
2019-02-06 20:53:56 +0100

    adding Task entity

---
+++ src/main/java/pl/rengreen/todolist/domain/Task.java
@@ -0,0 +1,94 @@
+package pl.rengreen.todolist.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String description;
+    private String startDate;
+    private String endDate;
+    private boolean isCompleted;
+    private String user;
+
+    public Task() {
+    }
+
+    public Task(String name, String description, String startDate, String endDate, boolean isCompleted) {
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isCompleted = isCompleted;
+    }
+
+    public Task(String name, String description, String startDate, String endDate, boolean isCompleted, String user) {
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isCompleted = isCompleted;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
