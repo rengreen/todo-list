5cbb7a5a0ab05637500cdbc40a257ef7fe5dfc16
rengreen <r.m.gardzinska@gmail.com>
2019-02-07 12:57:09 +0100

    adding index.html with its controller

---
+++ src/main/java/pl/rengreen/todolist/controller/IndexController.java
@@ -0,0 +1,13 @@
+package pl.rengreen.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    String index(){
+        return "index";
+    }
+}
