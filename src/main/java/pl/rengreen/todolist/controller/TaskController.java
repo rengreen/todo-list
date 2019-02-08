a41b855db2b9c90b934d41aac33bf369cda05485
rengreen <r.m.gardzinska@gmail.com>
2019-02-08 16:13:23 +0100

    adding taskList template and its controller

--- src/main/java/pl/rengreen/todolist/controller/TaskController.java
+++ src/main/java/pl/rengreen/todolist/controller/TaskController.java
@@ -1,13 +1,22 @@
 package pl.rengreen.todolist.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import pl.rengreen.todolist.service.TaskService;
 
-@RestController
+@Controller
 public class TaskController {
 
+    @Autowired
+    private TaskService taskService;
+
     @RequestMapping("/tasks/")
-    String text(){
-        return "text";
+    public String listUsers(Model model) {
+        model.addAttribute("tasks", taskService.findAll());
+        return "views/tasksList";
     }
 }
+
+
--- src/main/java/pl/rengreen/todolist/service/TaskService.java
+++ src/main/java/pl/rengreen/todolist/service/TaskService.java
@@ -1,11 +1,13 @@
 package pl.rengreen.todolist.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pl.rengreen.todolist.domain.Task;
 import pl.rengreen.todolist.repository.TaskRepository;
 
 import java.util.List;
 
+@Service
 public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
