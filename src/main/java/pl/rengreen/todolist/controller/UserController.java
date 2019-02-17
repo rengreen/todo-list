2e7049f98eff0b6af14a7c3657fa7c438baf6dea
rengreen <r.m.gardzinska@gmail.com>
2019-02-17 12:33:06 +0100

    adding Users List and its controller

---
+++ src/main/java/pl/rengreen/todolist/controller/UserController.java
@@ -0,0 +1,20 @@
+package pl.rengreen.todolist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.rengreen.todolist.service.UserService;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+   @GetMapping("/users")
+    public String listUsers(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "views/usersList";
+    }
+}
